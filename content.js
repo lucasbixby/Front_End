f636b3edaa2df5ae961d95a282271365dc8b437e
Lucas Bixby <lucasbixby04@gmail.com>
2025-01-18 23:38:42 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,5 @@
 console.log('Content script loaded on:', window.location.href);
+
+const element = document.getElementById("course_syllabus")
+const data = element ? element.innerText : null;
+crome.runtime.sendMessage({ action: "extract_data", data: data});
