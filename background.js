2a8da16b30b71327758a95d20d8678bd7ea28689
CalMoney11 <calvinnnpdobson@gmail.com>
2025-01-18 23:53:42 -0800

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension installed!');
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.data) {
+        const blob = new Blob([message.data], { type: "text/plain" });
+        const url = URL.createObjectURL(blob);
+
+        // Create a download link
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "extracted_data.txt";
+        a.click();
+
+        // Clean up
+        URL.revokeObjectURL(url);
+    }
 });
